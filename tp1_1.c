468f6d81dbee7528e4b424e15757971a47e54160
RudeVal <vrudellifacu@gmail.com>
2026-04-28 18:21:51 -0300

    tp1_1.c terminado

--- tp1_1.c
+++ tp1_1.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+
 int main (){
     printf("hola mundo\n");
+
+    int variable = 10, *puntero = &variable;
+    printf("Contenido del puntero: %d \n", *puntero);
+    printf("Direccion de memoria almacenada por el puntero: %p \n", puntero);
+    printf("Direccion de memoria de la variable: %p \n", &variable);
+    printf("Direccion de memoria del puntero: %p \n", &puntero);
+    printf("Tamano de memoria utilizado por esa variable: %zu \n", sizeof(variable));
+
+
+
+
+
     return 0;
 }
